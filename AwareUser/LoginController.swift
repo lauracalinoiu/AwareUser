f4b79e1fb1b0899477124f180dbeec3264223b47
Laura C <lauramocanu16@gmail.com>
2015-11-11 07:03:25 +0200

    TouchId for login

--- AwareUser/LoginController.swift
+++ AwareUser/LoginController.swift
@@ -8,17 +8,55 @@
 
 import UIKit
 
-class LoginController: UIViewController {
+class LoginController: UIViewController, TouchIDDelegate {
 
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var passTextField: UITextField!
     var storyboardMain: UIStoryboard?
+    @IBOutlet weak var lblMessage: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         storyboardMain = UIStoryboard.init(name: "Main", bundle: nil)
+        
+        let touchIDAuth : TouchID = TouchID()
+        touchIDAuth.touchIDReasonString = "To access the app."
+        touchIDAuth.delegate = self
+        touchIDAuth.touchIDAuthentication()
     }
-
+    func touchIDAuthenticationWasSuccessful() {
+        // TODO: Proceed to the app and show its contents after a successful login.
+        
+        self.lblMessage.text = "Successful Authentication!"
+    }
+    
+    
+    func touchIDAuthenticationFailed(errorCode: TouchIDErrorCode) {
+        // TODO: Fallback to a custom authentication method when necessary.
+        
+        switch errorCode{
+        case .TouchID_CanceledByTheSystem:
+            self.lblMessage.text = "Canceled by the system"
+            
+        case .TouchID_CanceledByTheUser:
+            self.lblMessage.text = "Canceled by the user"
+            
+        case .TouchID_PasscodeNotSet:
+            self.lblMessage.text = "No passcode was set"
+            
+        case .TouchID_TouchIDNotAvailable:
+            self.lblMessage.text = "TouchID is not available"
+            
+        case .TouchID_TouchIDNotEnrolled:
+            self.lblMessage.text = "No enrolled finger was found"
+            
+        case .TouchID_UserFallback:
+            self.lblMessage.text = "Should call custom authentication method"
+            
+        default:
+            self.lblMessage.text = "Authentication failed"
+        }
+    }
 
     @IBAction func submitPressed(sender: UIButton) {
         
